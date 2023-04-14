8562d0a8bc21e6f64bb01e28dfc91bc2527ef9c9
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-14 14:33:18 +0200

    homepage 1.1

--- spotify-with-redux/src/components/Homepage.jsx
+++ spotify-with-redux/src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
 
 const Homepage = () => {
   return (
+    <>
     <Container fluid>
       <Row>
         <Col md={2}>
@@ -84,7 +85,107 @@ const Homepage = () => {
             </div>
           </Nav>
         </Col>
+
+        <Col xs={12} md={9} class=" offset-md-3 mainPage">
+          <Row>
+            <Col xs={9} lg={11} class=" mainLinks d-none d-md-flex">
+              <a href="#">TRENDING</a>
+              <a href="#">PODCAST</a>
+              <a href="#">MOODS AND GENRES</a>
+              <a href="#">NEW RELEASES</a>
+              <a href="#">DISCOVER</a>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10}>
+              <div id="searchResults" style={{display: 'none'}}>
+                <h2>Search Results</h2>
+                <Row
+                  class="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                ></Row>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10}>
+              <div id="rock">
+                <h2>Rock Classics</h2>
+                <Row
+                  class="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                  id="rockSection"
+                ></Row>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10}>
+              <div id="pop">
+                <h2>Pop Culture</h2>
+                <Row
+                  class="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                  id="popSection"
+                ></Row>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10}>
+              <div id="hiphop">
+                <h2>#HipHop</h2>
+                <Row
+                  class="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                  id="hipHopSection"
+                ></Row>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+
       </Row>
     </Container>
+
+<Container fluid className=" fixed-bottom bg-container pt-1">
+<Row>
+  <Col lg={10 }className="offset-lg-2">
+    <Row>
+      <Col xs={6} md={4} lg={2}
+        className=" offset-3 offset-md-4 offset-lg-5 playerControls mt-1"
+      >
+        <Row>
+          <a href="#">
+            <img src="playerbuttons/Shuffle.png" alt="shuffle" />
+          </a>
+          <a href="#">
+            <img src="playerbuttons/Previous.png" alt="previous" />
+          </a>
+          <a href="#">
+            <img src="playerbuttons/Play.png" alt="play" />
+          </a>
+          <a href="#">
+            <img src="playerbuttons/Next.png" alt="next" />
+          </a>
+          <a href="#">
+            <img src="playerbuttons/Repeat.png" alt="repeat" />
+          </a>
+        </Row>
+      </Col>
+    </Row>
+    <Row className="justify-content-center playBar py-3">
+      <Col xs={8} md={6}>
+        <div class="progress">
+          <div
+            class="progress-bar"
+            role="progressbar"
+            aria-valuenow="0"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </Col>
+    </Row>
+  </Col>
+</Row>
+</Container>
+</>
   );
 };
