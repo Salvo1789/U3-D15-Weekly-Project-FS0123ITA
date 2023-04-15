e15d00c4878477c27af04bf55cbd9f933a41ba11
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-15 17:22:49 +0200

    1.1

--- spotify-with-redux/src/components/ArtistPage.jsx
+++ spotify-with-redux/src/components/ArtistPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import AlbumCard from './AlbumCard'
 
-const ArtistMain = () => {
+const ArtistPage = () => {
   const ARTIST_URL = 'https://striveschool-api.herokuapp.com/api/deezer/artist/'
   const SEARCH_URL = 'https://striveschool-api.herokuapp.com/api/deezer/search?q='
 
